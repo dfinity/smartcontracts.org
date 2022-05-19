19205ee4fab838a60ba077f03e21579155406fef
Akos Olasz <olaszakos@gmail.com>
2022-05-19 17:00:57 +0200

    gitpod integration

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,22 @@ const config = {
 		adapter: adapter(),
 		prerender: {
 			default: true
-		}
+		},
+		vite: {
+			server: {
+			  // Configure Vite for HMR with Gitpod.
+			  hmr: process.env.GITPOD_WORKSPACE_URL
+				? {
+					// Due to port fowarding, we have to replace
+					// 'https' with the forwarded port, as this
+					// is the URI created by Gitpod.
+					host: process.env.GITPOD_WORKSPACE_URL.replace("https://", "3000-"),
+					protocol: "wss",
+					clientPort: 443
+				  }
+				: true
+			}
+		  }
 	}
 };
 
